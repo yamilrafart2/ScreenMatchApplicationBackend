56e21170355e37ed8ddfd302ef00f340106c2ec0
yamilrafart2 <yamilrafart2>
2026-02-14 19:47:47 -0300

    Agrega la funcionalidad de buscar todas las series

--- src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -1,14 +1,26 @@
 package com.aluracursos.screenmatch.controller;
 
+import com.aluracursos.screenmatch.dto.SerieDTO;
+import com.aluracursos.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
 
+    @Autowired
+    private SerieRepository serieRepository;
+
     @GetMapping("/series")
-    public String mostrarMensaje() {
-        return "Este es mi primer mensaje en mi aplicación web!";
+    public List<SerieDTO> obtenerTodasLasSeries() {
+        return serieRepository.findAll().stream()
+                .map(s -> new SerieDTO(s.getTitulo(), s.getTotalTemporadas(), s.getEvaluacion(), s.getPoster(), s.getGenero(), s.getActores(), s.getSinopsis()))
+                .collect(Collectors.toList())
+        ;
     }
 
 }
