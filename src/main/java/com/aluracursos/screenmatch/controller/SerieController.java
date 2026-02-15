c0f88bd5f87b0406d49d307809fcfd1eb4413597
yamilrafart2 <yamilrafart2>
2026-02-14 21:08:26 -0300

    Agrega un servicio para distribuir responsabilidades

--- src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -1,26 +1,22 @@
 package com.aluracursos.screenmatch.controller;
 
 import com.aluracursos.screenmatch.dto.SerieDTO;
-import com.aluracursos.screenmatch.repository.SerieRepository;
+import com.aluracursos.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
 
     @Autowired
-    private SerieRepository serieRepository;
+    private SerieService servicio;
 
     @GetMapping("/series")
     public List<SerieDTO> obtenerTodasLasSeries() {
-        return serieRepository.findAll().stream()
-                .map(s -> new SerieDTO(s.getTitulo(), s.getTotalTemporadas(), s.getEvaluacion(), s.getPoster(), s.getGenero(), s.getActores(), s.getSinopsis()))
-                .collect(Collectors.toList())
-        ;
+        return servicio.obtenerTodasLasSeries();
     }
 
 }
---
+++ src/main/java/com/aluracursos/screenmatch/service/SerieService.java
@@ -0,0 +1,24 @@
+package com.aluracursos.screenmatch.service;
+
+import com.aluracursos.screenmatch.dto.SerieDTO;
+import com.aluracursos.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SerieService {
+
+    @Autowired
+    private SerieRepository serieRepository;
+
+    public List<SerieDTO> obtenerTodasLasSeries() {
+        return serieRepository.findAll().stream()
+                .map(s -> new SerieDTO(s.getTitulo(), s.getTotalTemporadas(), s.getEvaluacion(), s.getPoster(), s.getGenero(), s.getActores(), s.getSinopsis()))
+                .collect(Collectors.toList())
+                ;
+    }
+
+}
